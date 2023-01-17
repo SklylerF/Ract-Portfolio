04ee756db40a2322dc4f4615778052970f1eb3b9
Skyler Ferreira <sky_3489@icloud.com>
2023-01-16 19:38:53 -0800

    removed dates

--- src/components/education.js
+++ src/components/education.js
@@ -15,18 +15,13 @@ function Education() {
                         <div>Computer Science - Full Stack Developement</div>
                         <p>GPA: 3.0</p>
                     </div>
-                    <div class="resume-date text-md-right">
-                        <span class="text-primary">may 2022 - present(Expected End Date October 29th 2022)</span>
-                    </div>
+
                 </div>
 
                 <div class="resume-item d-flex flex-column flex-md-row">
                     <div class="resume-content mr-auto">
                         <h3 class="mb-1">Etiwanda High School</h3>
                         <div class="subheading mb-3">High School Deploma</div>
-                    </div>
-                    <div class="resume-date text-md-right">
-                        <span class="text-primary">August 2015 - May 2020</span>
                     </div>
                 </div>
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -16,9 +16,7 @@ function projects() {
                             secure password generator that can be accessed and used by the public. </p>
                         <a href="https://sklylerf.github.io/Danger-Zone/">Visit Danger-Zone!</a>
                     </div>
-                    <div class="resume-date text-md-right">
-                        <span class="text-primary">June 19th 2022 - Present</span>
-                    </div>
+
                 </div>
 
                 <div class="resume-item d-flex flex-column flex-md-row mb-5">
@@ -30,9 +28,7 @@ function projects() {
                             generator that can be downloaded from my GitHub page and used alongside the provided instructions. </p>
                         <a href="https://github.com/SklylerF/Professional-README-Generator">Visit Repo!</a>
                     </div>
-                    <div class="resume-date text-md-right">
-                        <span class="text-primary">July 10th 2022 - July 31st 2022</span>
-                    </div>
+
                 </div>
 
                 <div class="resume-item d-flex flex-column flex-md-row">
@@ -42,9 +38,6 @@ function projects() {
                         <p>In this project, I worked alone and demonstrated skills like Javascript, HTML5, and CSS to create a
                             secure password generator that can be accessed and used by the public. </p>
                         <a href="https://sklylerf.github.io/Passeord-Generator/">Visit Password Generator!</a>
-                    </div>
-                    <div class="resume-date text-md-right">
-                        <span class="text-primary">June 5th 2022 - Present</span>
                     </div>
                 </div>
 
